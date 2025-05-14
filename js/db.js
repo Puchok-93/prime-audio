2720c3a167f8d4c898b6219f082540e013d042d7
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2025-05-14 15:29:56 +0300

    init prime audio

---
+++ js/db.js
@@ -0,0 +1,218 @@
+const brands = [
+    {
+        name: 'Noble Audio',
+        link: 'noble-audio',
+        backgroundImg: 'bg-noble-audio.jpg',
+        logoImg: 'logo-noble.png'
+    },
+    {
+        name: 'Meters',
+        link: 'meters',
+        backgroundImg: 'bg-meters.jpg',
+        logoImg: 'logo-meters.png'
+    },
+    {
+        name: 'Tula',
+        link: 'tula',
+        backgroundImg: 'bg-tula.jpg',
+        logoImg: 'logo-tula.png'
+    },
+    {
+        name: 'Amphion',
+        link: 'amphion',
+        backgroundImg: 'bg-amphion.jpg',
+        logoImg: 'logo-amphion.png'
+    },
+    {
+        name: 'Antelope Audio',
+        link: 'antelope-audio',
+        backgroundImg: 'bg-antelope-audio.jpg',
+        logoImg: 'logo-antelope.webp'
+    },
+    {
+        name: 'Kinera',
+        link: 'kinera',
+        backgroundImg: 'bg-kinera.jpg',
+        logoImg: 'logo-kinera.png'
+    },
+    {
+        name: 'Dekoni Audio',
+        link: 'dekoni-audio',
+        backgroundImg: 'bg-dekoni-audio.jpg',
+        logoImg: 'logo-dekoni.png'
+    },
+    {
+        name: 'Ultimate Ears',
+        link: 'ultimate-ears',
+        backgroundImg: 'bg-ultimate-ears.jpg',
+        logoImg: 'logo-ultimate-ears.png'
+    },
+    {
+        name: 'Dutch&Dutch',
+        link: 'dutch-dutch',
+        backgroundImg: 'bg-dutch-dutch.jpg',
+        logoImg: 'logo-dutch.png'
+    },
+    {
+        name: 'ddHiFi',
+        link: 'ddhifi',
+        backgroundImg: 'bg-ddhifi.jpg',
+        logoImg: 'logo-ddhifi.png'
+    },
+    {
+        name: 'Dan Clark Audio',
+        link: 'dan_clark_audio',
+        backgroundImg: 'bg-dan-clark-audio.jpg',
+        logoImg: 'logo-dc-audio.png'
+    },
+    {
+        name: 'Stockfisch Records',
+        link: 'stockfisch_records',
+        backgroundImg: 'bg-stockfisch.jpg',
+        logoImg: 'logo-stockfish.png'
+    },
+    {
+        name: 'Hidizs',
+        link: 'hidizs',
+        backgroundImg: 'bg-hidizs.jpg',
+        logoImg: 'logo-hidizs.png'
+    },
+    {
+        name: 'Shanling',
+        link: 'shanling',
+        backgroundImg: 'bg-shanling.jpg',
+        logoImg: 'logo-shanling.png'
+    },
+    {
+        name: 'Lotoo',
+        link: '',
+        backgroundImg: 'bg-lotoo.jpg',
+        logoImg: 'logo-lotoo.png'
+    },
+    {
+        name: 'Lake People',
+        link: 'lake_people',
+        backgroundImg: 'bg-lake-people.jpg',
+        logoImg: 'logo-lake-people.png'
+    },
+    {
+        name: 'Violectric',
+        link: 'violectric',
+        backgroundImg: 'bg-violectric.jpg',
+        logoImg: 'logo-violectric.png'
+    },
+    {
+        name: 'PMC',
+        link: 'pmc',
+        backgroundImg: 'bg-pmc.jpg',
+        logoImg: 'logo-pmc.png'
+    },
+    {
+        name: 'Knowledge Zenith',
+        link: 'knowledge_zenith',
+        backgroundImg: 'bg-kz.jpg',
+        logoImg: 'logo-kz.png'
+    },
+    {
+        name: 'Abyss',
+        link: 'abyss',
+        backgroundImg: 'bg-abyss.jpg',
+        logoImg: 'logo-abyss.png'
+    },
+    {
+        name: 'Unique Melody',
+        link: 'unique_melody',
+        backgroundImg: 'bg-unique-melody.jpg',
+        logoImg: 'logo-unique-melody.png'
+    },
+    {
+        name: 'Cayin',
+        link: 'cayin',
+        backgroundImg: 'bg-cayin.jpg',
+        logoImg: 'logo-cayin.png'
+    },
+    {
+        name: 'Fir Audio',
+        link: 'fir_audio',
+        backgroundImg: 'bg-fir-audio.jpg',
+        logoImg: 'logo-firaudio.png'
+    },
+    {
+        name: 'Nobunaga Labs',
+        link: 'nobunaga-labs',
+        backgroundImg: 'bg-nobunaga-labs.jpg',
+        logoImg: 'logo-nobunaga.png'
+    },
+    {
+        name: 'Klatre',
+        link: 'klatre',
+        backgroundImg: 'bg-klatre.jpg',
+        logoImg: 'logo-klatre.png'
+    },
+    {
+        name: 'Crescendo',
+        link: 'crescendo',
+        backgroundImg: 'bg-crescendo.jpg',
+        logoImg: 'logo-crescendo.png'
+    },
+    {
+        name: 'Campfire Audio',
+        link: 'campfire-audio',
+        backgroundImg: 'bg-campfire-audio.jpg',
+        logoImg: 'logo-campfire.png'
+    },
+    {
+        name: 'Hiby',
+        link: 'hiby',
+        backgroundImg: 'bg-hiby.jpg',
+        logoImg: 'logo-hiby.png'
+    },
+    {
+        name: 'Sieveking Sound',
+        link: 'sieveking-sound',
+        backgroundImg: 'bg-sieveking-sound.jpg',
+        logoImg: 'logo-.svg'
+    },
+    {
+        name: 'Furutech',
+        link: 'furutech',
+        backgroundImg: 'bg-furutech.jpg',
+        logoImg: 'logo-furutech.png'
+    },
+    {
+        name: 'Final Audio',
+        link: 'final-audio',
+        backgroundImg: 'bg-final-audio.jpg',
+        logoImg: 'logo-final.png'
+    },
+    {
+        name: 'Soyuz',
+        link: 'soyuz',
+        backgroundImg: 'bg-soyuz.jpg',
+        logoImg: 'logo-soyuz.png'
+    },
+    {
+        name: 'ZMF',
+        link: 'zmf',
+        backgroundImg: 'bg-zmf.jpg',
+        logoImg: 'logo-zmf.svg'
+    },
+    {
+        name: 'Simpleway Audio',
+        link: 'simpleway',
+        backgroundImg: 'bg-simpleway.jpg',
+        logoImg: 'logo-simpleway.png'
+    },
+    {
+        name: 'Leiyin Audio',
+        link: '',
+        backgroundImg: 'bg-leiyin-audio.jpg',
+        logoImg: 'logo-leiyien.png'
+    },
+    {
+        name: 'MoonDrop',
+        link: 'moondrop',
+        backgroundImg: 'bg-moondrop.jpg',
+        logoImg: 'logo-.svg'
+    },
+]
---
+++ js/script.js
@@ -0,0 +1,126 @@
+function initMultipleSlider() {
+    const multipleSlider = document.getElementById('multiple-slider');
+
+    if(!multipleSlider) {
+        return
+    }
+
+    const multipleSliderWrapper = multipleSlider.querySelector('.swiper-wrapper');
+    const banners = document.querySelectorAll('.banner');
+    const sControlsTemplate = `
+		<div class="swiper-pagination-controls">
+			<div class="swiper-button-prev">
+				<svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<rect x="0.5" y="15.5" width="15" height="15" rx="7.5" transform="rotate(-90 0.5 15.5)"/>
+					<path d="M9 11L6 8L9 5" stroke-linecap="round" stroke-linejoin="round"/>
+				</svg>
+			</div>
+			<div class="swiper-pagination"></div>
+			<div class="swiper-button-next">
+				<svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<rect x="0.5" y="15.5" width="15" height="15" rx="7.5" transform="rotate(-90 0.5 15.5)"/>
+					<path d="M7 5L10 8L7 11" stroke-linecap="round" stroke-linejoin="round"/>
+				</svg>
+			</div>
+		</div>
+	`
+
+    if(banners.length > 3 || window.innerWidth < 767 && banners.length > 1) {
+        multipleSliderWrapper.classList.remove('catalog-banner-list');
+        multipleSlider.insertAdjacentHTML('beforeend', sControlsTemplate);
+
+        const slider = new Swiper(multipleSlider, {
+            simulateTouch: true,
+            spaceBetween: 15,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            breakpoints: {
+                1024: {
+                    slidesPerView: 3,
+                },
+                768: {
+                    slidesPerView: 2,
+                }
+            }
+        })
+    }
+    console.log('test')
+}
+
+function initBrands() {
+    const obBrandList = document.querySelector('.brand-list');
+    const sBrandTemplate = ({name, link, backgroundImg, logoImg}) =>
+        `
+            <li class="brand" style="background: url(../img/${backgroundImg}) center / cover">
+                <a class="brand__link" href="https://drhead.ae/brands/${link}/">
+                    <img class="brand-info__img" src="img/${logoImg}" alt="${name}">
+                </a>
+                <a class="brand__sub-link" href="https://drhead.ae/brands/${link}">View more</a>
+            </li>
+        `
+    ;
+
+    const render = () => {
+        obBrandList.innerHTML = '';
+        const createBrandsString = brands.map(item => sBrandTemplate(item)).join('');
+        obBrandList.insertAdjacentHTML('beforeend', createBrandsString);
+    }
+
+    render();
+}
+
+function initMobNavigation() {
+    const navigation = document.querySelector('.navigation');
+    const navigationBtn = document.querySelector('#burger-mob-navigation');
+
+    navigationBtn.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if(target) {
+            navigation.classList.toggle('js-active');
+            navigationBtn.classList.toggle('js-active');
+        }
+    })
+}
+
+function initSmoothScroll() {
+    const anchors = document.querySelectorAll('.anchor');
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault()
+            const blockID = anchor.getAttribute('href').substr(1);
+            let offset = 100;
+            window.scrollTo({
+                behavior: 'smooth',
+                top:
+                    document.getElementById(blockID).getBoundingClientRect().top -
+                    document.body.getBoundingClientRect().top - offset,
+            })
+
+            if(window.innerWidth < 768) {
+                document.querySelector('.navigation').classList.remove('js-active');;
+                document.querySelector('#burger-mob-navigation').classList.remove('js-active');;
+            }
+        })
+    }
+}
+
+function init() {
+    initMultipleSlider();
+    initBrands();
+    initSmoothScroll();
+    if(window.innerWidth < 767) {
+        initMobNavigation();
+    }
+
+    console.log(brands)
+}
+
+window.addEventListener('DOMContentLoaded', init);
